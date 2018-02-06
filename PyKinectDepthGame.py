aa957570b907722f8669ec2906775986cf9013fa
Unknown <raikuz53@gmail.com>
2018-02-06 14:42:46 +0100

    Il suffisait d'un self

--- PyKinectDepthGame.py
+++ PyKinectDepthGame.py
@@ -31,95 +31,95 @@ SKELETON_COLORS = [pygame.color.THECOLORS["red"],
 
 
 class depthRuntime(object):
-    def __init__(self):
-            pygame.init()
+	def __init__(self):
+			pygame.init()
 
     # Used to manage how fast the screen updates
-            self._clock = pygame.time.Clock()
+			self._clock = pygame.time.Clock()
 
     # Loop until the user clicks the close button.
-            self._done = False
+			self._done = False
 
     # Used to manage how fast the screen updates
-            self._clock = pygame.time.Clock()
+			self._clock = pygame.time.Clock()
 
     # Kinect runtime object, we want only color and body frames 
-            self._kinect = PyKinectRuntime.PyKinectRuntime(PyKinectV2.FrameSourceTypes_Depth)
+			self._kinect = PyKinectRuntime.PyKinectRuntime(PyKinectV2.FrameSourceTypes_Depth)
 
     # back buffer surface for getting Kinect depth frames, 8bit grey, width and height equal to the Kinect color frame size
-            self._frame_surface = pygame.Surface((self._kinect.depth_frame_desc.Width, self._kinect.depth_frame_desc.Height), 0, 24)
+			self._frame_surface = pygame.Surface((self._kinect.depth_frame_desc.Width, self._kinect.depth_frame_desc.Height), 0, 24)
     # here we will store skeleton data 
-            self._bodies = None
+			self._bodies = None
     
     # Set the width and height of the screen [width, height]
-            self._infoObject = pygame.display.Info()
-            self._screen = pygame.display.set_mode((self._kinect.depth_frame_desc.Width, self._kinect.depth_frame_desc.Height), pygame.HWSURFACE|pygame.DOUBLEBUF|pygame.RESIZABLE, 32)
+			self._infoObject = pygame.display.Info()
+			self._screen = pygame.display.set_mode((self._kinect.depth_frame_desc.Width, self._kinect.depth_frame_desc.Height), pygame.HWSURFACE|pygame.DOUBLEBUF|pygame.RESIZABLE, 32)
 
-            pygame.display.set_caption("Kinect for Windows v2 depth")
+			pygame.display.set_caption("Kinect for Windows v2 depth")
             
-    def classifier_homemade(self, frameArray):
-            filelistCisor = glob.glob('EchantillonCiseaux/*.png')
-            filelistPaper = glob.glob('EchantillonPapier/*.png')
-            filelistRock = glob.glob('EchantillonPierre/*.png')
+	def classifier_homemade(self, frameArray):
+			filelistCisor = glob.glob('EchantillonCiseaux/*.png')
+			filelistPaper = glob.glob('EchantillonPapier/*.png')
+			filelistRock = glob.glob('EchantillonPierre/*.png')
 
-            npArrayCisor = np.array([np.array(Image.open(fname)) for fname in filelistCisor])
-            npArrayPaper = np.array([np.array(Image.open(fname)) for fname in filelistPaper])
-            npArrayRock = np.array([np.array(Image.open(fname)) for fname in filelistRock])
+			npArrayCisor = np.array([np.array(Image.open(fname)) for fname in filelistCisor])
+			npArrayPaper = np.array([np.array(Image.open(fname)) for fname in filelistPaper])
+			npArrayRock = np.array([np.array(Image.open(fname)) for fname in filelistRock])
 
-            clfCisor = NearestCentroid(metric='euclidean', shrink_threshold=None)
-            clfCisor.fit(filelistCisor, frameArray)
-            print("Cisor : "+clf.predict([[-0.8, -1]]))
+			clfCisor = NearestCentroid(metric='euclidean', shrink_threshold=None)
+			clfCisor.fit(filelistCisor, frameArray)
+			print("Cisor : "+clf.predict([[-0.8, -1]]))
 
-            clfPaper = NearestCentroid(metric='euclidean', shrink_threshold=None)
-            clfPaper.fit(filelistPaper, frameArray)
-            print("Paper : "+clf.predict([[-0.8, -1]]))
+			clfPaper = NearestCentroid(metric='euclidean', shrink_threshold=None)
+			clfPaper.fit(filelistPaper, frameArray)
+			print("Paper : "+clf.predict([[-0.8, -1]]))
 
-            clfRock = NearestCentroid(metric='euclidean', shrink_threshold=None)
-            clfRock.fit(filelistRock, frameArray)
-            print("Rock : "+clf.predict([[-0.8, -1]]))
+			clfRock = NearestCentroid(metric='euclidean', shrink_threshold=None)
+			clfRock.fit(filelistRock, frameArray)
+			print("Rock : "+clf.predict([[-0.8, -1]]))
             
-    def draw_depth_frame(self, frame, target_surface):
-            if frame is None:  # some usb hub do not provide the depth image. it works with Kinect studio though
-                    return
-            target_surface.lock()
-            f8=np.uint8(frame.clip(1,4000)/16.)
-            new_f8 = [np.uint8(250) if x > np.uint8(40) and x < np.uint8(70) else np.uint8(0) for x in f8]
-            classifier_homemade(new_f8)
-            frame8bit=np.dstack((new_f8,new_f8,new_f8))
-            address = self._kinect.surface_as_array(target_surface.get_buffer())
-            ctypes.memmove(address, frame8bit.ctypes.data, frame8bit.size)
-            del address
-            target_surface.unlock()
+	def draw_depth_frame(self, frame, target_surface):
+			if frame is None:  # some usb hub do not provide the depth image. it works with Kinect studio though
+					return
+			target_surface.lock()
+			f8 = np.uint8(frame.clip(1,4000)/16.)
+			new_f8 = [np.uint8(250) if x > np.uint8(40) and x < np.uint8(70) else np.uint8(0) for x in f8]
+			self.classifier_homemade(new_f8)
+			frame8bit=np.dstack((new_f8,new_f8,new_f8))
+			address = self._kinect.surface_as_array(target_surface.get_buffer())
+			ctypes.memmove(address, frame8bit.ctypes.data, frame8bit.size)
+			del address
+			target_surface.unlock()
             
 
-    def run(self):
+	def run(self):
     # -------- Main Program Loop -----------
-            while not self._done:
+			while not self._done:
         # --- Main event loop
-                    for event in pygame.event.get(): # User did something
-                            if event.type == pygame.QUIT: # If user clicked close
-                                    self._done = True # Flag that we are done so we exit this loop
+					for event in pygame.event.get(): # User did something
+							if event.type == pygame.QUIT: # If user clicked close
+									self._done = True # Flag that we are done so we exit this loop
 
-                            elif event.type == pygame.VIDEORESIZE: # window resized
-                                    self._screen = pygame.display.set_mode(event.dict['size'], pygame.HWSURFACE|pygame.DOUBLEBUF|pygame.RESIZABLE, 32)
+							elif event.type == pygame.VIDEORESIZE: # window resized
+									self._screen = pygame.display.set_mode(event.dict['size'], pygame.HWSURFACE|pygame.DOUBLEBUF|pygame.RESIZABLE, 32)
         # --- Getting frames and drawing  
-                    if self._kinect.has_new_depth_frame():
-                            frame = self._kinect.get_last_depth_frame()
-                            self.draw_depth_frame(frame, self._frame_surface)
-                            frame = None
+					if self._kinect.has_new_depth_frame():
+							frame = self._kinect.get_last_depth_frame()
+							self.draw_depth_frame(frame, self._frame_surface)
+							frame = None
 
-                    self._screen.blit(self._frame_surface, (0,0))
-                    pygame.display.update()
+					self._screen.blit(self._frame_surface, (0,0))
+					pygame.display.update()
 
         # --- Go ahead and update the screen with what we've drawn.
-                    pygame.display.flip()
+					pygame.display.flip()
 
         # --- Limit to 60 frames per second
-                    self._clock.tick(60)
+					self._clock.tick(60)
 
     # Close our Kinect sensor, close the window and quit.
-            self._kinect.close()
-            pygame.quit()
+			self._kinect.close()
+			pygame.quit()
 
 
 __main__ = "Kinect v2 depth"
